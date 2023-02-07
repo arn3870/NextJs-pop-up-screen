072aeda4ee44a61916712f6a2d45457cc5f609d5
Arslan Baig <arn3870@gmail.com>
2023-02-07 17:59:04 +0500

    styling updates

--- src/components/FinalPage.js
+++ src/components/FinalPage.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import styles from "./css/FinalPage.module.css";
 import style from "./css/generalStyling.module.css";
 
-const FinalPage = (handleNext, handlePrev, handleClose) => {
+const FinalPage = ({handleNext, handlePrev, handleClose}) => {
   return (
     <div className={style.wrapper}>
       <div className={styles.textClass}>Almost Done! <br/> Where is this project taking place?</div>
-      <div className={style.cancel } onclick={handleClose} />
-      <div className={style.leftArrow} onclick={handlePrev} />
+      <div className={style.cancel } onClick={handleClose} />
+      <div className={style.leftArrow} onClick={handlePrev} />
       <div className={styles.Rectangle6} />
       <div className={styles.Rectangle7} />
       <div className={style.modalClass} />
