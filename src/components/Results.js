e8ba7c19b17648101f6ddbb5e1697099353d4865
Arslan Baig <arn3870@gmail.com>
2023-02-12 00:07:30 +0500

    Result Page Updates

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,24 +1,25 @@
 import React from "react";
-import style from "./css/generalStyling.module.css";
 import styles from "./css/Results.module.css";
 import BaseCard from "./BaseCard";
 
-const Results = ({ handleNext, handlePrev, handleClose, values }) => {
+const Results = ({ handlePrev, values }) => {
   return (
     <BaseCard>
       <div className={styles.leftArrow} onClick={handlePrev} />
       <p className={styles.p}>{values[1]}</p>
-      <br></br>
+      <br />
       <p className={styles.p}>{values[2]}</p>
-      <br></br>
+      <br />
       <p className={styles.p}>{values[3]}</p>
-      <br></br>
+      <br />
       <p className={styles.p}>{values[4]}</p>
-      <br></br>
+      <br />
       <p className={styles.p}>{values[5]}</p>
-      <br></br>
-      <p className={styles.p}>{values[6]}</p>
-      <br></br>
+      <br />
+      <p className={styles.p}>{values[6][0]}</p>
+      <br />
+      <p className={styles.p}>{values[6][1].toString()}</p>
+      <br />
     </BaseCard>
   );
 };
